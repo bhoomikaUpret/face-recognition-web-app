87bf1a10b9c176c29dfd4f7fcce0c5ea6c90969a
bhoomikaUpret <112538577+bhoomikaUpret@users.noreply.github.com>
2023-02-02 18:48:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ Webcam.attach('#camera');
 
 function takeSnapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("resutl").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
     });
 }
 
@@ -22,3 +22,24 @@ function modelLoaded()
 {
     console.log('Model Loaded!')
 }
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult());
+}
+
+function gotResult(error, results)
+{
+    if (error){
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
+
+
+
+
